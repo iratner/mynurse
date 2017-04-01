dd727ffc973c7b6470719556f0234d36a06a78cd
Feras Deiratany <deiratany.1@osu.edu>
2017-04-01 17:14:21 -0400

    signup form

---
+++ source/js/data.js
@@ -0,0 +1,15 @@
+var nurseDropdowns = {
+    "specialty": [
+        { "speciality_id":"1", "Name":"Generalist" },
+        { "speciality_id":"1", "Name":"Psychiatric" },
+        { "speciality_id":"1", "Name":"Home health" },
+        { "speciality_id":"1", "Name":"School" },
+        { "speciality_id":"1", "Name":"Neurotrauma" }
+    ],
+    "experience" : [
+        { "yrs_xp_id":"1", "yrs_xp":"less than 5"},
+        { "yrs_xp_id":"2", "yrs_xp":"5-10"},
+        { "yrs_xp_id":"3", "yrs_xp":"10-15"},
+        { "yrs_xp_id":"4", "yrs_xp":"15+"}
+    ]
+};

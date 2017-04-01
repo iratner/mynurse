d20cbe778f9e5ba25d10c9c1503292da17338a00
Feras Deiratany <deiratany.1@osu.edu>
2017-04-01 15:39:55 -0400

    adding nurse signup

---
+++ source/js/partials.js
@@ -0,0 +1,9 @@
+Handlebars.registerPartial("container", Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<div id=\"container\">\n\n</div>\n\n\n";
+},"useData":true}));
+Handlebars.registerPartial("header", Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<div class=\"header\">\n    <div class=\"title\">My Family Nurse (replace with logo)</div>\n</div>\n<div class=\"navbar\">\n    <ul class=\"navbar-right\">\n        <li>Login</li>\n        <li>Sign Up</li>\n    </ul>\n    <ul class=\"navbar-left\">\n        <li>Map</li>\n        <li>FAQ</li>\n        <li>Contact</li>\n    </ul>\n</div>";
+},"useData":true}));
+Handlebars.registerPartial("nurse-signup", Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<div class=\"nurse-signup\">\n    \n</div>";
+},"useData":true}));

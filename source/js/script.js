9c24fee7212ac75ba84fb862a9650de5388b66a7
Ilya <iratner81@gmail.com>
2017-04-01 10:16:49 -0400

    First commit

---
+++ source/js/script.js
@@ -0,0 +1,21 @@
+var Nurses = (function() {
+
+    console.log("hey there!");
+
+})(jQuery);
+
+
+// Handlebars examples
+
+/*Handlebars.registerHelper('list', function(items, options) {
+    var out = "<ul>";
+
+    for(var i=0, l=items.length; i<l; i++) {
+        out = out + "<li>" + options.fn(items[i]) + "</li>";
+    }
+
+    return out + "</ul>";
+});
+
+
+Handlebars.partials['container']({id : "id"});*/
